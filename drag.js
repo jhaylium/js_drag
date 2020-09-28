55dac9082079cd739e3378489dd3f3ab5eb78b32
Jeff <jmccain@haylium.com>
2020-09-28 00:10:08 -0400

    Added method to insert element in correct place

--- drag.js
+++ drag.js
@@ -14,7 +14,27 @@ draggables.forEach(draggable =>{
 containers.forEach(container =>{
     container.addEventListener('dragover', drag_event =>{
         drag_event.preventDefault();
+        const afterElement = getDragAfterElement(container, drag_event.clientY)
         const draggable = document.querySelector('.dragging');
-        container.appendChild(draggable);
+        if (afterElement == null){
+            container.appendChild(draggable);
+        } else {
+            container.insertBefore(draggable, afterElement)
+        }
     })
 })
+
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+  
+    return draggableElements.reduce((closest, child) => {
+      const bounding_box = child.getBoundingClientRect()
+      const offset = y - bounding_box.top - bounding_box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+  }
